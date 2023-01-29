aa2b1eeff651273c1cacf2fbb3fe652dc1be20c0
AnaisGalisson-ChillStudio <anais.galisson.chill.studio@gmail.com>
2023-01-29 20:31:03 +0100

    feat(ref): add first focus on creditCardNumber input

--- src/component/CreditCart/CreditCard.jsx
+++ src/component/CreditCart/CreditCard.jsx
@@ -1,20 +1,44 @@
+import { useRef } from "react";
 import s from "./style.module.css";
 
 export function CreditCard() {
+  const cardCodeInputRef = useRef();
+  const cardExpirationInputRef = useRef();
+
+  const onCardNumberChangeInput = (event) => {
+    if (event.target.value.length === 16) {
+      cardCodeInputRef.current.focus();
+    }
+  };
+
   return (
     <div className={s.container}>
       Credit Card
       <div>
         <label>Code Carte Bancaire</label>
-        <input type="number" name="creditCardNumber" />
+        <input
+          onChange={onCardNumberChangeInput}
+          type="number"
+          name="creditCardNumber"
+        />
       </div>
       <div>
         <label>Code Secret </label>
-        <input type="number" name="creditCardCode" />
+        <input
+          ref={(reference) => {
+            cardCodeInputRef.current = reference;
+          }}
+          type="number"
+          name="creditCardCode"
+        />
       </div>
       <div>
         <label>Expiration</label>
-        <input type="text" name="creditCardExpiratio " />
+        <input
+          ref={cardExpirationInputRef}
+          type="text"
+          name="creditCardExpiratio "
+        />
       </div>
     </div>
   );
